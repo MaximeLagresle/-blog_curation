35dfdab6a77df2f4cfdd8ebb2677b5fe71ee4df9
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-04-05 17:03:53 +0200

    blogs modal set up

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
 
   get 'pages/news', to: 'pages#news'
 
+  # get 'pages', to: 'blogs#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
