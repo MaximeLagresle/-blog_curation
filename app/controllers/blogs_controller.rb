2a6ea2f68bd55130e05e2b5b60eb8e8df4a2e6ec
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-04-19 19:20:23 +0200

    posts ajax setup

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,17 +1,9 @@
 class BlogsController < ApplicationController
   before_action :authenticate_user!
 
-  def list
-    @all_blogs = Blog.all
-  end
-
-  # def show
-  #   # Initialize instance variables
-  #   @blog = Blog.find(params[:id])
-  # end
-
   def follow
     set_blog
+    set_feed
     current_user.follow(@blog)
     @follow = Follow.find_by(follower: current_user, followable: @blog)
     respond_to do |format|
@@ -22,6 +14,7 @@ class BlogsController < ApplicationController
 
   def unfollow
     set_blog
+    set_feed
     current_user.stop_following(@blog)
     respond_to do |format|
       format.html { redirect_to root_path }
@@ -33,5 +26,14 @@ class BlogsController < ApplicationController
 
   def set_blog
     @blog = Blog.find(params[:id])
+  end
+
+  def set_feed
+    @all_blogs = Blog.all
+    @all_posts = Post.all.order('published desc')
+
+    @my_blogs = current_user.all_following
+
+    @my_posts = @all_posts.select { |post| @my_blogs.include?(post.blog) }
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
+require 'pry-byebug'
+
 class PagesController < ApplicationController
   def home
-    @all_posts = Post.all.order('published desc')
     @all_blogs = Blog.all
+    @all_posts = Post.all.order('published desc')
+
+    @my_blogs = current_user.all_following
+
+    @my_posts = @all_posts.select { |post| @my_blogs.include?(post.blog) }
   end
 end
