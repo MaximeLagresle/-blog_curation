a2140b767446dd3a67c5936b96cd62b3f493bf98
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-04-24 17:53:42 +0200

    styling home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,20 @@
 require 'pry-byebug'
 
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
     @all_blogs = Blog.all
     @all_posts = Post.all.order('published desc')
 
-    @my_blogs = current_user.all_following
 
-    @my_posts = @all_posts.select { |post| @my_blogs.include?(post.blog) }
+    if user_signed_in?
+      @my_blogs = current_user.all_following
+      @my_posts = @all_posts.select { |post| @my_blogs.include?(post.blog) }
+    else
+      @my_blogs = [Blog.find(2), Blog.find(1)]
+      @my_posts = @all_posts.select { |post| @my_blogs.include?(post.blog) }
+      # binding.pry
+    end
   end
 end
