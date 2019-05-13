565e3ec68b0443630c040ef6ef5cf536396f22f4
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-05-13 19:48:09 +0200

    devise _links signup / signin as modals

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   # get 'pages', to: 'blogs#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :blogs, only: [:list] do
     member do
       post :follow
